765b084b71ab31b6c915e8299082520085d11ebc
Robert Preda <predarobert7@gmail.com>
2021-08-18 02:36:13 +0300

    Initial commit

---
+++ froggson.py
@@ -0,0 +1,46 @@
+import time
+import discord
+from discord.ext import commands
+from random import randrange
+
+from discord.utils import get
+from discord import Role
+from random import shuffle
+
+from discord.ext.commands import Bot
+
+
+token = 'ODc2MTEyOTc1OTQ0MTA2MDQ0.YRfVyg.y7pHsDi9p-1fEymD4hU168wrja4'
+
+bot = discord.Client()
+welcome_msg_id = 876102121219588187
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == welcome_msg_id:
+        reactor = payload.user_id
+        if payload.emoji.name == '✅':
+            role_id = 876139733565198377
+            role = get(payload.member.guild.roles, id=role_id)
+            await payload.member.add_roles(role)
+        elif payload.emoji.name == '🔞':
+            role_id = 876104434348851252
+            role = get(payload.member.guild.roles, id=role_id)
+            await payload.member.add_roles(role)
+
+
+        
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    if payload.message_id == welcome_msg_id:
+        reactor = payload.user_id
+        if payload.emoji.name == '✅':
+            role_id = 876104434348851252
+            role = get(payload.member.guild.roles, id=role_id)
+            await payload.member.remove_roles(role)
+bot.run(token)
