f0f2c5e5f02f984ecb441b1d07d168716a5f3f29
Robert Preda <predarobert7@gmail.com>
2021-08-18 18:00:36 +0300

    Cleaned some code, added role map, removing roles works now

--- froggson.py
+++ froggson.py
@@ -11,8 +11,18 @@ from discord.ext.commands import Bot
 from constants import token
 
 
-bot = discord.Client()
+intents = discord.Intents.all()
+bot = discord.Client(intents=intents)
 welcome_msg_id = 876102121219588187
+
+
+roles_id_map = {
+    '✅': 876139733565198377,
+    '🔞': 876104434348851252,
+    '🧱': 877320383236288542,
+    'valorant': 877548236410343465,
+    'league': 877547947955470357
+}
 
 @bot.event
 async def on_ready():
@@ -21,14 +31,17 @@ async def on_ready():
 @bot.event
 async def on_raw_reaction_add(payload):
     if payload.message_id == welcome_msg_id:
-        reactor = payload.user_id
-        if payload.emoji.name == '✅':
-            role_id = 876139733565198377
+        ########## FOR DEBUGGING PURPOSES ##########
+        ########## UNCOMMENT IF NEEDED #############
+        # print(payload.emoji.name)
+        ############################################
+        try:
+            role_id = roles_id_map[payload.emoji.name]
             role = get(payload.member.guild.roles, id=role_id)
-            await payload.member.add_roles(role)
-        elif payload.emoji.name == '🔞':
-            role_id = 876104434348851252
-            role = get(payload.member.guild.roles, id=role_id)
+        except KeyError:
+            role = None
+
+        if role != None:
             await payload.member.add_roles(role)
 
 
@@ -36,10 +49,16 @@ async def on_raw_reaction_add(payload):
 
 @bot.event
 async def on_raw_reaction_remove(payload):
+    guild  = bot.get_guild(payload.guild_id)
+    member = guild.get_member(payload.user_id)
     if payload.message_id == welcome_msg_id:
-        reactor = payload.user_id
-        if payload.emoji.name == '✅':
-            role_id = 876104434348851252
-            role = get(payload.member.guild.roles, id=role_id)
-            await payload.member.remove_roles(role)
+        try:
+            role_id = roles_id_map[payload.emoji.name]
+            role = get(guild.roles, id=role_id)
+        except KeyError:
+            role = None
+        # if payload.emoji.name == '✅':
+        #     role_id = 876104434348851252
+        #     role = get(payload.member.guild.roles, id=role_id)
+        await member.remove_roles(role)
 bot.run(token)
